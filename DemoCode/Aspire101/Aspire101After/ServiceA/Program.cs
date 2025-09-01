0a2e6fd019d2974cd5dca7a38f5f720560306f52
Kaj Bromose <kbr@ucl.dk>
2025-09-01 17:07:55 +0200

    Aspire101After - Aspire added

--- DemoCode/Aspire101/Aspire101After/ServiceA/Program.cs
+++ DemoCode/Aspire101/Aspire101After/ServiceA/Program.cs
@@ -2,16 +2,26 @@ using ServiceA;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.AddServiceDefaults();
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+//builder.Services.AddHttpClient<IServiceBProxy, ServiceBProxy>(client =>
+//{
+//    client.BaseAddress = new Uri("https://localhost:7233/");
+//});
+
+// Use Aspire-injected endpoint for serviceb
 builder.Services.AddHttpClient<IServiceBProxy, ServiceBProxy>(client =>
 {
-    client.BaseAddress = new Uri("https://localhost:7233/");
+    client.BaseAddress = new Uri("https+http://serviceb");
 });
 
 var app = builder.Build();
+
+app.MapDefaultEndpoints();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) app.MapOpenApi();
